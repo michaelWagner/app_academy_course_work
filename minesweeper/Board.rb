692e28d06c842854487d59cece800af53fa659c8
Michael Wagner <wagnerdmike@gmail.com>
2015-04-28 20:28:18 -0700

    Change bombed to boolean value.

--- minesweeper/Board.rb
+++ minesweeper/Board.rb
@@ -2,7 +2,7 @@ require_relative 'Tile'
 require_relative 'Game'
 
 class Board
-  MAX_MINES = (9 * 9) / 3
+  MAX_MINES =  5 # (9 * 9) / 3
   attr_reader :total_bomb_count, :grid_size
   attr_accessor :minesweeper_board
 
@@ -13,14 +13,21 @@ class Board
 
   def build_board(grid_size)
     @total_bomb_count = 0
+    bombed = false
     minesweeper_board = Array.new(grid_size) { Array.new(grid_size) }
     minesweeper_board.each_index do |y|
       minesweeper_board.each_index do |x|
-        bombed = rand(2)
-        @total_bomb_count += 1 if bombed == 1
-        minesweeper_board[y][x] = Tile.new(self, bombed, [y,x])
+        if @total_bomb_count < MAX_MINES
+          bombed = [false, true].sample
+          p bombed
+          if bombed == true
+            @total_bomb_count += 1
+          end
+        end
+        minesweeper_board[y][x] = Tile.new(self, [y,x], bombed)
       end
     end
+    puts @total_bomb_count
     minesweeper_board
   end
 
@@ -29,30 +36,24 @@ class Board
 
       minesweeper_board.each_index do |x|
         tile = minesweeper_board[y][x]
-
         unless tile.flagged
-
           unless tile.revealed
             print " * "
           else
-
-            if tile.bombed == 1
+            if tile.bombed
               print " B "
             elsif tile.neighbor_bomb_count > 0
               print " #{tile.neighbor_bomb_count} "
             else
               print " _ "
             end
-
           end
         else
           print " F "
         end
-
       end
       print "\n"
     end
     nil
   end
-
 end
