9922d26cf6de02fc4019ce7ae74ca2a7639d16d3
michaelwagner <wagnerdmike@gmail.com>
2015-05-11 18:05:23 -0700

    TODO bonus.

--- first_routes_app/app/models/contact_share.rb
+++ first_routes_app/app/models/contact_share.rb
@@ -2,5 +2,6 @@ class ContactShare < ActiveRecord::Base
   validates :contact_id, :user_id, presence: true, uniqueness: true
   belongs_to :contact
   belongs_to :user
+  has_many :comments
 
 end
--- first_routes_app/app/models/user.rb
+++ first_routes_app/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :contacts, dependent: :destroy
   has_many :contact_shares, dependent: :destroy
   has_many :shared_contacts, through: :contact_shares, source: :contact
-
+  has_many :comments, as: :commentable
 end
