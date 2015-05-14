67d27eb28fd4b6471485ab265d3ade2a690528b3
Michael Wagner <wagnerdmike@gmail.com>
2015-05-14 11:17:09 -0700

    Initial commit for music app.

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :generate_session_token
+
+  def generate_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+end
